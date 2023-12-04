adfc0cb3d6cb7888805b63b443d6ad88acea2aaf
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-12-03 21:09:52 -0500

    Clocked sort and reverted back to using data structures

--- data.py
+++ data.py
@@ -1,13 +1,24 @@
 from google.cloud import bigquery
+from google.oauth2 import service_account
+import os
 
 def fetch_data():
-    client = bigquery.Client(project='161603559480')
+    # Path to the service account key file
+    key_path = os.path.join(os.path.dirname(__file__), 'cop3530-final-cece71eb5bc6.json')
+
+    # Load credentials from the key file
+    credentials = service_account.Credentials.from_service_account_file(key_path)
+
+    # Initialize the BigQuery Client with the credentials
+    client = bigquery.Client(credentials=credentials, project=credentials.project_id)
+
     query = """
     SELECT case_number, date, primary_type, description, location_description, arrest, domestic, CAST(district AS INT64) AS district, year
     FROM `bigquery-public-data.chicago_crime.crime`
     WHERE year BETWEEN 2015 AND 2023
-    LIMIT 10000
+    LIMIT 100000
     """
+
     query_job = client.query(query)
     results = query_job.to_dataframe()
     return results
